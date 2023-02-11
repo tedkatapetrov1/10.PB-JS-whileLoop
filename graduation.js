4d4095f08073f7b819ec956fa5354b3b2e30d990
Teodor Petrov <teodor.petrov@abv.bg>
2023-02-11 15:32:53 +0200

    new file

--- graduation.js
+++ graduation.js
@@ -1,17 +1,46 @@
 function graduation(input) {
+    let index = 0;
+    let name = input[index];
+    index++
+    let year = 1;
+    let sumGrade = 0;
+    let excluded = 0
+    let expelled = false;
+
+    while (year <= 12) {
+        let currentGrade = Number(input[index]);
+        index++;
+        if (currentGrade <= 4) {
+            excluded++;
+            if (excluded === 2) {
+                expelled = true;
+                break;
+
+            }
+            continue;
+        }
+        sumGrade = sumGrade + currentGrade;
+        year++;
+    }
+    if (expelled == false) {
+        let averageGrade = sumGrade / 12;
+        console.log(`${name} graduated. Average grade: ${averageGrade.toFixed(2)}`);
+    } else {
+        console.log(`${name} has been excluded at ${year} grade`);
+    }
 
 }
 
-graduation(["Gosho",
-    "5",
-    "5.5",
-    "6",
-    "5.43",
-    "5.5",
-    "6",
-    "5.55",
-    "5",
-    "6",
-    "6",
-    "5.43",
-    "5"])
+graduation(["Gosho", 
+"5",
+"5.5",
+"6",
+"5.43",
+"5.5",
+"6",
+"5.55",
+"5",
+"6",
+"6",
+"5.43",
+"5"])
